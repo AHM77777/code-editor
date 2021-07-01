95578794a14ffa78030e5e4585de8eead28b8770
RoninGil <carlos7397@hotmail.com>
2021-07-01 17:08:11 -0500

    Added Home button on NavBar. Added some style to Code Editor page. Added NextRouter. Changed names from Login to NavBar.

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -2,7 +2,7 @@ import Editor from '../Components/Editor/Editor'
 import 'codemirror/lib/codemirror.css'
 import { useEffect, useState } from "react";
 import styles from '../Components/Files.module.scss'
-import  Login  from "../Components/Login/Login"
+import  NavBar  from "../Components/NavBar/NavBar"
 
 const EditorPage = () => {
 
@@ -42,7 +42,7 @@ const EditorPage = () => {
     }, [html, css, js])
 
     return ( <>
-    <Login />
+    <NavBar />
         <div className={styles.mainContainer}>
             
             <div className={styles.leftPage}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
-import  Login  from "../Components/Login/Login"
+import  NavBar  from "../Components/NavBar/NavBar"
 import { useRouter } from 'next/router'
 
 function HomePage() {
   const router = useRouter()
-
-  return <>
-  <Login />
-
   const handleClick = (e) => {
     e.preventDefault()
     router.push('/EditorPage')
   }
+  return <>
+  <NavBar />
+
+  
 
   <button onClick={handleClick}>Nuevo</button> <button>Cargar </button>
   </>
